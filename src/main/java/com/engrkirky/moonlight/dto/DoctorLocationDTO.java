eba79052affb96fdefaab06ffc2bf5e3bf9ad96f
Kirk Santos <kirkalyn13@gmail.com>
2024-08-11 21:45:49 +0800

    feat: serialize loc dto

--- src/main/java/com/engrkirky/moonlight/dto/DoctorLocationDTO.java
+++ src/main/java/com/engrkirky/moonlight/dto/DoctorLocationDTO.java
@@ -3,9 +3,11 @@ package com.engrkirky.moonlight.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
-public class DoctorLocationDTO {
+public class DoctorLocationDTO implements Serializable {
     private double longitude;
     private double latitude;
 }
