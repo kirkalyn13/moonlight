c0a49ccafe76990b51f34609ae211979ba9f1b17
Kirk Santos <kirkalyn13@gmail.com>
2024-08-11 15:39:00 +0800

    initial commit

---
+++ src/test/java/com/engrkirky/moonlight/MoonlightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engrkirky.moonlight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoonlightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
